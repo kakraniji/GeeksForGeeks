fc5eead316f5b06430e1a425199b6a76b53a4514
kakraniji <60791683+kakraniji@users.noreply.github.com>
2020-10-30 19:09:53 +0530

    Create Fast Power .cpp

---
+++ Recursion/Fast Power .cpp
@@ -0,0 +1,16 @@
+  
+//this will calculate the power in log(n) time..
+int power(int a,int b)
+{
+	if(b==0)
+	return 1;
+	
+	int smallans=power(a,b/2);
+	smallans=smallans*smallans;
+	
+	//agar b odd he to a ka multiply bi hoga smallans me!!! else smallans return ho jayga!!
+	if(b&1)
+	return a*smallans;
+
+	return smallans;
+}
