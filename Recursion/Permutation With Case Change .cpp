4a8bc0544a9c2c4fbf3fb9ccbe73fadb10683c44
kakraniji <60791683+kakraniji@users.noreply.github.com>
2020-10-31 12:29:34 +0530

    Create Permutation With Case Change .cpp

---
+++ Recursion/Permutation With Case Change .cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+char casechange(char ch)
+{
+    ch=ch-32;
+    return ch;
+}
+void solve(string s,string ans)
+{
+    if(s.size()==0)
+    {
+        cout<<ans<<endl;
+        return;
+    }
+    
+    char ch=s[0];
+    string roq=s.substr(1);
+    
+    solve(roq,ans+casechange(ch));
+    solve(roq,ans+ch);
+}
+int main() {
+	
+	    string s;
+	    cin>>s;
+	    
+	    solve(s,"");
+}
